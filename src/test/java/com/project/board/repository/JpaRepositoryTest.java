c0fc9113556e16501a14e4471d9e1a25d30f207b
wndus-lee <wndus6093@naver.com>
2022-12-18 15:30:12 +0900

    #12 - jpa test 작성
    기본 crud + 연관 관계 잘 작동하지는 보려고 작성
    테스트 데이터와 관련이 있기 때문에 설계

---
+++ src/test/java/com/project/board/repository/JpaRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.project.board.repository;
+
+import com.project.board.config.JpaConfig;
+import com.project.board.domain.Article;
+import com.project.board.domain.ArticleComment;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ActiveProfiles("testdb")
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class)
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(
+            @Autowired ArticleRepository articleRepository,
+            @Autowired ArticleCommentRepository articleCommentRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select 테스트")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine(){
+        //Given
+        long previousCount = articleRepository.count();
+        //When
+        List<Article> articles = articleRepository.findAll();
+
+        //Then
+        assertThat(articles)
+                .isNotNull()
+                .hasSize(0);
+
+    }
+}
