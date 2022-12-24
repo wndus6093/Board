856b016655a4ee4da8ebb9a6d6d48a6f7316c42e
wndus-lee <wndus6093@naver.com>
2022-12-24 21:29:20 +0900

    Revert "#21 - 댓글 기능 구현"
    This reverts commit f10c29366efd5a786a52aa297879c683b2edba0e.

--- src/main/java/service/ArticleCommentService.java
+++ src/main/java/service/ArticleCommentService.java
@@ -1,19 +1,14 @@
 package service;
 
-import com.project.board.domain.ArticleComment;
 import com.project.board.dto.ArticleCommentDto;
 import com.project.board.repository.ArticleCommentRepository;
 import com.project.board.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
-import static java.lang.Math.log;
-@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -24,31 +19,15 @@ public class ArticleCommentService {
 
     @Transactional(readOnly = true)
     public List<ArticleCommentDto> searchArticleComments(Long articleId) {
-        return articleCommentRepository.findByArticle_Id(articleId)
-                .stream()
-                .map(ArticleCommentDto::from)
-                .toList();
+        return List.of();
     }
 
-    public void saveArticleComment(ArticleCommentDto dto) {
-        try {
-            articleCommentRepository.save(dto.toEntity(articleRepository.getReferenceById(dto.articleId())));
-        } catch (EntityNotFoundException e) {
-            log.warn("댓글 저장 실패. 댓글의 게시글을 찾을 수 없습니다 - dto: {}", dto);
-        }
+    public void saveArticleComment(ArticleCommentDto dto){
     }
 
-    public void updateArticleComment(ArticleCommentDto dto) {
-        try {
-            ArticleComment articleComment = articleCommentRepository.getReferenceById(dto.id());
-            if (dto.content() != null) { articleComment.setContent(dto.content()); }
-        } catch (EntityNotFoundException e) {
-            log.warn("댓글 업데이트 실패. 댓글을 찾을 수 없습니다 - dto: {}", dto);
-        }
+    public void updateArticleComment(ArticleCommentDto dto){
     }
 
-    public void deleteArticleComment(Long articleCommentId) {
-        articleCommentRepository.deleteById(articleCommentId);
+    public void deleteArticleComment(Long dto){
     }
-
 }
